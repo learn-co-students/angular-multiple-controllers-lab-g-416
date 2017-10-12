69d64d01fcec0d89e1912c5e36cf0977bd0f2c7f
lizdenhup <lizdenhup@gmail.com>
2017-10-12 15:49:19 -0500

    Done.

---
+++ js/app/controllers/ContactController.js
@@ -0,0 +1,14 @@
+function ContactController() {
+    var vm = this;
+    vm.name = 'Steve Jobs';
+    vm.email = 'steve@apple.com';
+    vm.phone = '0987654321';
+
+    vm.changeName = function() {
+        vm.name = 'Tim Cook';
+    };
+};
+
+angular 
+    .module('app')
+    .controller('ContactController', ContactController);
