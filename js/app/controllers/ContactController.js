2392ad8ec3e681da3b1dac665cc936f6082d1351
VictoriaMeng <1ststarofthenight@gmail.com>
2019-03-22 10:18:17 -0700

    Call setters on vm to add name, email, phone keys to ContactController()

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,5 +1,8 @@
 function ContactController() {
   var vm = this;
+  vm.name = "Me";
+  vm.email = "me@gmail.com";
+  vm.phone = "800-111-1111"
 
   this.changeName = function() {
     vm.name = 'Something else!';
