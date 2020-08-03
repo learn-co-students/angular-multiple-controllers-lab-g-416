9387bbae17b35c9827cdb14198709987a875d384
Paul Rosero <paul_rosero@yahoo.com>
2020-08-03 18:02:04 -0400

    Done.

---
+++ js/app/controllers/ContactController.js
@@ -0,0 +1,9 @@
+function ContactController($scope){
+    name = "Paul";
+    email = "paul@yahoo.com";
+    phone = "234-234-5456"
+}
+
+angular
+    .module('app');
+    .controller('ContactController', ContactController);
